02cdd07889c6de0415a1519a0d932d5a967b70ca
MrTroble <nifroh56@gmail.com>
2020-11-03 14:20:26 +0100

    Added Julia Test

---
+++ Physik/JuliaTest.jl
@@ -0,0 +1,27 @@
+```
+# Induktion
+U_ind = -n ⋅ dΦ/dt
+Φ = B ⋅ A
+
+1.3 Induktion durch Änderung einer Fläche
+2 Induktion durch Änderung des Magnetfeldes
+
+* Versuch mit zwei in einander liegenden Spulen
+* Die äusere Spule (Feldspule) ist an eine Variable Stromquelle angeschlossen
+* Die innere Spule (Induktionsspule) ist an ein Strommesser angeschlossen
+* Die äusere Spule induziert der inneren Spule Strom -> Der Spannungsmesser schlägt aus
+
+Eine Spannung wird induziert da die ein Magnetfeld entsteht, dieses wiederum bewegt
+die elektrone druch die änderung der Feldlinien, durch die Richtungsänderungen. Es entsteht ein
+sich änderndes Magnetfeld
+
+> Die Induktion ensteht durch Feldlinien änderungen
+
+Nenne Alle Eigenschaften der Feld und Induktionsspannung begründe anhand des Induktionsgesetzes
+```
+
+f(x) = (x/2) % 2 ? (2-2 ⋅ x) : (2 ⋅ x)
+plot(:8, f)
+
+gui()
+wait()
